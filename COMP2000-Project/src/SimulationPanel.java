6360544eea3a91788260d019034fb9696c2bb734
R-Reagan-K <raphaelreaganrusli.kencana@students.mq.edu.au>
2026-09-05 17:43:06 +1000

    Spaceship's sprite now rotates according to angle

--- COMP2000-Project/src/SimulationPanel.java
+++ COMP2000-Project/src/SimulationPanel.java
@@ -1,6 +1,7 @@
 import java.awt.Panel;
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Graphics2D;
 
 public class SimulationPanel extends Panel{
     private Spaceship ship;
@@ -31,14 +32,29 @@ public class SimulationPanel extends Panel{
         g.fillOval(earthX, earthY, earth.size, earth.size);
 
         
-        g.setColor(Color.RED);
-        int shipCenterX = (int) ship.coordinates[0];
-        int shipCenterY = (int) ship.coordinates[1];
-        int shipHalf = ship.size/2;
-        int[]shipXPoints = { shipCenterX, shipCenterX - shipHalf, shipCenterX + shipHalf };
-        int[]shipYPoints = { shipCenterY - shipHalf, shipCenterY + shipHalf, shipCenterY + shipHalf };
+        // g.setColor(Color.RED);
+        // int shipCenterX = (int) ship.coordinates[0];
+        // int shipCenterY = (int) ship.coordinates[1];
+        // int shipHalf = ship.size/2;
+        // int[]shipXPoints = { shipCenterX, shipCenterX - shipHalf, shipCenterX + shipHalf };
+        // int[]shipYPoints = { shipCenterY - shipHalf, shipCenterY + shipHalf, shipCenterY + shipHalf };
 
-        g.fillPolygon(shipXPoints, shipYPoints, 3);
+        // g.fillPolygon(shipXPoints, shipYPoints, 3);
+        Graphics2D g2d = (Graphics2D) g.create();
+
+        int shipX = (int) ship.coordinates[0];
+        int shipY = (int) ship.coordinates[1];
+        int shipHalf = ship.size/2;
+
+        g2d.translate(shipX, shipY);
+        g2d.rotate(ship.angle);
+        g2d.setColor(Color.RED);
+
+        int[] shipPointsX = { shipHalf, -shipHalf, -shipHalf };
+        int[] shipPointsY = { 0, -shipHalf, shipHalf };
+        
+        g2d.fillPolygon(shipPointsX, shipPointsY, 3);
+        g2d.dispose();
 
     }
     
--- COMP2000-Project/src/tempCodeRunnerFile.java
+++ COMP2000-Project/src/tempCodeRunnerFile.java
@@ -1,8 +1,2 @@
-or.RED);
-        int shipCenterX = (int) ship.coordinates[0];
-        int shipCenterY = (int) ship.coordinates[1];
-        int shipHalf = ship.size/2;
-        int[]shipXPoints = { shipCenterX, shipCenterX - shipHalf, shipCenterX + shipHalf };
-        int[]shipYPoints = { shipCenterY - shipHalf, shipCenterY + shipHalf, shipCenterY + shipHalf };
 
-        g.fillPolygon(sh
+    
