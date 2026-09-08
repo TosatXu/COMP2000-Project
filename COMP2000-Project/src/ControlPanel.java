6a6c95c018e2793e8b0351b3f267c7766d55af24
Caridee <hoilam.chau@students.mq.edu.au>
2026-09-08 22:00:04 +0800

    Modified the control panel

--- COMP2000-Project/src/ControlPanel.java
+++ COMP2000-Project/src/ControlPanel.java
@@ -11,27 +11,65 @@ public class ControlPanel extends Panel {
     public Button resetButton;
 
     public ControlPanel() {
-        setLayout(new GridLayout(5,1, 5, 5));
+        setLayout(null);
+        setBackground(new Color(25, 25, 40));
+        
+        // Title label
+        Label title = new Label("Spaceship Control Panel");
+        title.setBounds(20, 30, 250, 25);
+        title.setForeground(Color.WHITE);
+        title.setFont(new Font("Arial", Font.BOLD, 18));
+        add(title);
 
-        add(new Label("Launch Angle (0 - 90)"));
+        // Launch Angle label and field
+        Label angleLabel = new Label("Launch Angle (0 - 90)");
+        angleLabel.setBounds(20, 80, 170, 20);
+        angleLabel.setForeground(Color.WHITE);
+        add(angleLabel);
+
+        
         angleField = new TextField("45");
+        angleField.setBounds(20, 105, 180, 25);
         add(angleField);
 
-        add(new Label("Engine Force (1 - 100)"));
+        // Engine Force label and field
+        Label forceLabel = new Label("Engine Force (1 - 100)");
+        forceLabel.setBounds(20, 150, 170, 20);
+        forceLabel.setForeground(Color.WHITE);
+        add(forceLabel);
 
         forceField = new TextField("40");
+        forceField.setBounds(20, 175, 180, 25);
         add(forceField);
 
         engineButton = new Button("Fire Engine");
+        engineButton.setBounds(20, 230, 100, 30);
         add(engineButton);
 
         startButton = new Button("Start");
+        startButton.setBounds(20, 290, 100, 30);
         add(startButton);
 
         pauseButton = new Button("Pause");
+        pauseButton.setBounds(20, 350, 100, 30);
         add(pauseButton);
 
         resetButton = new Button("Reset");
+        resetButton.setBounds(20, 410, 100, 30);
         add(resetButton);
+    }
+
+    @Override 
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        g.setColor(Color.WHITE);
+
+        g.drawLine(0, 0, 0, getHeight());
+    }
+
+    @Override 
+    public Dimension getPreferredSize() {
+        return new Dimension(300, 600);
     }
 }
